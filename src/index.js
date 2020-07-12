7b66d205e46d3bcaa16d22069dd00d032339658c
Sergei Shinkarenko <serewja@gmail.com>
2020-07-12 13:16:53 +0500

    add func

---
+++ src/index.js
@@ -0,0 +1,11 @@
+export default (num) => {
+  if (num <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= num / 2; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
