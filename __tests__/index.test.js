c6d36780db2f246b6076704aade60317d5c2d624
Sergei Shinkarenko <serewja@gmail.com>
2020-07-12 13:04:04 +0500

    add tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,8 @@
-import f from "../dist/index.js"
+import isPrime from "../dist/index.js"
 
 test('testname', () => {
-    
+  expecet(isPrime(1)).toBe(false);
+  expecet(isPrime(7)).toBe(true);
+  expecet(isPrime(10)).toBe(false);
+  expecet(isPrime(-7)).toBe(false);
 });
